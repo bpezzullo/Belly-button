049f7025c1af979c791676adb12dec6622e532a9
Bill Pezzullo <bpezzullo@gmail.com>
2020-09-14 21:04:28 -0400

    added gauge and readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Plotly-challenge
-Producing charts through Plotly
+# Plotly - Belly Button Biodiversity
+
+![Bacteria by filterforge.com](Images/bacteria.jpg)
+
+Built an interactive dashboard to explore the [Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonize human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+# Plotly Dashboards
+
+Top half of the dashboard showing the ability to create a drop down and use the input to populate the plots.
+The data changes based on the subject selected.
+
+  ![Dashboard Chart](Images/dashboard.png)
+
+Bottom half of display showing the bubble chart plot.
+
+  ![Dashboard Chart](Images/dashboard2.png)
+
+Access the page by going to https://bpezzullo.github.io/Plotly-challenge/
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,73 +1,116 @@
-
+/*
+  Initalizae the drop down tags for interactive display.  Initalize the demographics and plots 
+  using the first subject.  Subsequent plots will be with restyle.  
+*/
 
 function init() {
 // pull the ids for the drop down
 
   d3.json("./data/samples.json").then(function(jsdata) {
-  var listIds = jsdata.samples.map(subjectid => subjectid.id);
-  var demographics = jsdata.metadata[0];
-  // console.log(listIds);
-  //Create the drop down list of subject IDs
-  document.getElementById("selDataset").innerHTML = generatetxt(listIds);
+    var listIds = jsdata.samples.map(subjectid => subjectid.id);
+    var demographics = jsdata.metadata[0];
+    // console.log(listIds);
+    //Create the drop down list of subject IDs
+    document.getElementById("selDataset").innerHTML = generatetxt(listIds);
 
-  // Display the demographics for the initial subject
-  document.getElementById("sample-metadata").innerHTML = generatedemo(demographics);
-  
-  // set up the data for the plots using the first subject.
-  var otuIds = jsdata.samples[0].otu_ids;
-  var otuValues = jsdata.samples[0].sample_values;
-  var otuLabels = jsdata.samples[0].otu_labels;
+    // Display the demographics for the initial subject
+    document.getElementById("sample-metadata").innerHTML = generatedemo(demographics);
 
-  // produce a pie chart with labels and values
-  var trace1 = 
+    var subjectText = `<h2 class="text-center">Subject: ${listIds[0]} Results</h2>`;
+    document.getElementById("subject-header").innerHTML = subjectText;
+    
+    // set up the data for the plots using the first subject.
+    var otuIds = jsdata.samples[0].otu_ids;
+    var otuValues = jsdata.samples[0].sample_values;
+    var otuLabels = jsdata.samples[0].otu_labels;
+
+    // produce a pie chart with labels and values
+    var trace1 = 
+      {
+        labels: otuIds.slice(0,10).reverse(),
+        values: otuValues.slice(0,10).reverse(),
+        type: 'pie'
+
+      };
+
+    // set up the layout
+    var layoutp = {
+      title: `Top 10 OTUs`,
+      height: 400,
+      width: 500
+      };
+
+    var datapie = [trace1];
+
+    // put a pie chart at the html class bar location
+    Plotly.newPlot('bar', datapie,layoutp);
+
+    // now create the bubble chart
+    var trace2 = 
     {
-      labels: otuIds.slice(0,10).reverse(),
-      values: otuValues.slice(0,10).reverse(),
-      type: 'pie'
-
+      x: otuIds,
+      y: otuValues,
+      text: otuLabels,
+      autocolorscale: true,
+      //colorscale: 'Jet',
+      marker: {
+        color: otuIds,
+        size: otuValues},
+      mode: 'markers'
     };
 
-  // set up the layout
-  var layoutp = {
-    title: `Top 10 OTUs for Subject ${jsdata.names[0]}`,
-    height: 400,
-    width: 500
-    };
+    // format the layout
+    var layoutb = {
 
-  var datapie = [trace1];
+      yaxis: { title: "OTU Value"},
+      xaxis: { title: "OTU ID"}
+      };
 
-  // put a pie chart at the html class bar location
-  Plotly.newPlot('bar', datapie,layoutp);
+    // format the data input
+    var databub = [trace2];
 
-  // now create the bubble chart
-  var trace2 = 
-  {
-    x: otuIds,
-    y: otuValues,
-    text: otuLabels,
-    colorscale: 'Jet',
-    marker: {
-      color: otuIds,
-      size: otuValues},
-    mode: 'markers'
-  };
+    // plot the bubble chart  
+    Plotly.newPlot('bubble', databub,layoutb);
 
-var layoutb = {
+    // set up the data for the gauge
+    var dataguage = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: demographics.wfreq,
+        title: { text: "Belly Button Washing" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { range:[0,9]},
+          steps: [
+            {range: [0,1], color: "white"},
+            {range: [1,2], color: "linen"},
+            {range: [2,3], color: "tan"},
+            {range: [3,4], color: "white"},
+            {range: [4,5], color: "linen"},
+            {range: [5,6], color: "tan"},
+            {range: [6,7], color: "white"},
+            {range: [7,8], color: "linen"},
+            {range: [8,9], color: "tan"}
+          ]
+        }
+      }
+    ];
 
-  yaxis: { title: "OTU Value"},
-  xaxis: { title: "OTU ID"}
-  };
+    var layoutguage = { width: 600, height: 500, margin: { t: 0, b: 0 } };
 
-var databub = [trace2];
-
-  
-Plotly.newPlot('bubble', databub,layoutb);
-});
+    // Plot the gauge
+    Plotly.newPlot('gauge', dataguage, layoutguage);
+  });
 }
 
-/**
- * Fetch data and build the timeseries plot
- */
+/*
+  Find the new subject and populate the demographics and plots 
+  based on that using restyle.  
+  Inputs
+    value - integer from the drop down. 
+
+*/
 
 function buildPlot(value) {
   var otuReferId;
@@ -115,7 +158,7 @@ function buildPlot(value) {
     // rebuild the plot
     Plotly.restyle('bar', 'labels', [otuIds.slice(0,10).reverse()]);
     Plotly.restyle('bar', 'values', [otuValues.slice(0,10).reverse()]);
-    Plotly.restyle('bar', 'title', [`Top 10 OTUs for Subject ${value}`])
+    //Plotly.restyle('bar', 'layoutp.title', [`Top 10 OTUs for Subject ${parseInt(value)}`])
 
 
     // redraw bubble chart for new subject
@@ -128,9 +171,26 @@ function buildPlot(value) {
     // Update the demographics for the new subject
     var demographics = jsdata.metadata[otuReferId];
     document.getElementById("sample-metadata").innerHTML = generatedemo(demographics);
+
+    // Update the header.
+    var subjectText = `<h2 class="text-center">Subject: ${value} Results</h2>`;
+    document.getElementById("subject-header").innerHTML = subjectText;
+
+    // Update the guage for the subject.
+    Plotly.restyle('gauge', 'value', [demographics.wfreq]);
   });
 }
-// function to generate the text for the drop downs.
+
+/* function to generate the text for the drop downs.  This function creates a text string
+  used to populate the drop down. 
+  Input:
+    keylist - array of integers containing the subject ID
+
+  Returns:
+    text - text string with html encoding with the format of 
+      <option> integer </option>
+
+*/
 
 function generatetxt(keylist) {
   // set up variables being used in function.
@@ -143,23 +203,42 @@ function generatetxt(keylist) {
   return text
 } 
 
-// fucntion to generate the text for the demographics panel.
+/* function to generate the text for the demographics panel.
+  Input:
+    subjectDemo: key value providing the demographics of the subject
+      id: Integer
+      ethnicity: tet field
+      gender: single character
+      age: integer
+      location: text field
+      bbtype:  Single character
+      wfreq:  integrer between 0 and 9
+  Returns:
+    text - text string with html encoding
+
+*/
 
 function generatedemo(subjectDemo) {
   // set up variables being used in function.
   var text = [];
 
-  // loop through array to populate the drop down.
-  text += '<br>Subject ID: ' + subjectDemo.id + '</br>';
-  text += '<br>Ethnicity: ' + subjectDemo.ethnicity + '</br>';
-  text += '<br>Gender: ' + subjectDemo.gender + '</br>';
-  text += '<br>Age: ' + subjectDemo.age + '</br>';
-  text += '<br>Location: ' + subjectDemo.location + '</br>';
-  text += '<br>BB Type: ' + subjectDemo.bbtype + '</br>';
-  text += '<br>W Freq.: ' + subjectDemo.wfreq + '</br>';
+  // populate the demographics Info Panel.
+  text += '<p class="font-weight-bold">Subject ID: ' + subjectDemo.id + '</p>';
+  text += '<p class="font-weight-bold">Ethnicity: ' + subjectDemo.ethnicity + '</p>';
+  text += '<p class="font-weight-bold">Gender: ' + subjectDemo.gender + '</p>';
+  text += '<p class="font-weight-bold">Age: ' + subjectDemo.age + '</p>';
+  text += '<p class="font-weight-bold">Location: ' + subjectDemo.location + '</p>';
+  text += '<p class="font-weight-bold">BB Type: ' + subjectDemo.bbtype + '</p>';
+  text += '<p class="font-weight-bold">W Freq.: ' + subjectDemo.wfreq + '</p>';
   return text
 }
+
 // Handle the selection of the new subject id from the drop down
+/* function to generate the text for the demographics panel.
+  Input:
+    value - information from the drop down field. Integer 
+
+*/
 function optionChanged(value) {
   buildPlot(value);
   
